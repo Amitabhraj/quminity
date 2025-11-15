b9ffb423b9860e228631cf893557cef736c02538
Amitabh Raj <abhiraj1709w@gmail.com>
2025-11-16 03:42:17 +0530

    Added some new changes

--- Qapp/admin.py
+++ Qapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser,Section,Course
 
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        ("Additional Info", {"fields": ("qid", "anonymous")}),
+        ("Additional Info", {"fields": ("qid", "anonymous","section")}),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Section)
+admin.site.register(Course)
--- Qapp/models.py
+++ Qapp/models.py
@@ -2,19 +2,55 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+courses_name_choices = [
+    ('B.tech CSE', 'B.tech CSE'),
+    ('B.tech Civil', 'B.tech Civil'),
+    ('B.tech AI/ML', 'B.tech AI/ML'),
+    ('B.tech Mechanical', 'B.tech Mechanical'),
+    ('BCA', 'BCA'),
+    ('MCA', 'MCA'),
+]
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100, choices=courses_name_choices,default="",null=True, blank=True)
+
+    def __str__(self):  
+        return self.course_name
+    
+
+class Subjects(models.Model):
+    course = models.ForeignKey(Course,on_delete=models.CASCADE, null=True, blank=True)
+    subject_name = models.CharField(default="",null=True,blank=True,max_length=100)
+    subject_code = models.CharField(default="",null=True,blank=True,max_length=100)
+
+    def __str__(self):
+        return f"{self.subject_name} - {self.subject_code}"
+    
+
+
+class Section(models.Model):
+    section_name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE, null=True, blank=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['section_name', 'course'], 
+                name='unique_section_per_course'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.section_name} - {self.course}"
+
 class CustomUser(AbstractUser):
     qid = models.CharField(max_length=15, unique=True, null=True, blank=True)
     anonymous = models.BooleanField(default=True,null=True, blank=True)
+    section = models.ForeignKey(Section,on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.username
     
     def get_full_name(self):   
         return f"{self.first_name} {self.last_name}"
-
-class Features(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
