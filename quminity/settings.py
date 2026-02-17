5479ed2b1b8921de3830702ddfd1738771ccc621
Amitabh Raj <abhiraj1709w@gmail.com>
2026-02-17 17:41:51 +0530

    Added some changes

--- quminity/settings.py
+++ quminity/settings.py
@@ -32,14 +32,17 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'localhost:8000',
     "https://d28xp4w96lfabk.cloudfront.net",
-    "http://ec2-13-232-185-226.ap-south-1.compute.amazonaws.com:8000"
+    "http://ec2-13-232-185-226.ap-south-1.compute.amazonaws.com"
 ]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://localhost:8000',
-    "http://ec2-13-232-185-226.ap-south-1.compute.amazonaws.com:8000",
+    "http://ec2-13-232-185-226.ap-south-1.compute.amazonaws.com",
     "https://d28xp4w96lfabk.cloudfront.net",
 ]
 
